73224c91d7a7331d6451ea5789f190dd9fd8dd5b
Saffura Ahamed <haiesaffura@gmail.com>
2023-06-04 23:16:17 +0530

    remove src folder

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const path = require('path')
-const staticPath = path.join(__dirname,"../public")
+const staticPath = path.join(__dirname,"/public")
 
 app.use(express.static(staticPath))
 
